36a38564a581ff1fff8fb207f7679b58330a6e74
rakibul-wdp <rakibul.wdp@gmail.com>
2024-01-31 21:26:01 +0600

    import images and export it

---
+++ public/assets/images/pll/index.js
@@ -0,0 +1,45 @@
+import aa from "./aa.png";
+import ab from "./ab.png";
+import e from "./e.png";
+import f from "./f.png";
+import ga from "./ga.png";
+import gb from "./gb.png";
+import gc from "./gc.png";
+import gd from "./gd.png";
+import h from "./h.png";
+import ja from "./ja.png";
+import jb from "./jb.png";
+import na from "./na.png";
+import nb from "./nb.png";
+import ra from "./ra.png";
+import rb from "./rb.png";
+import t from "./t.png";
+import ua from "./ua.png";
+import ub from "./ub.png";
+import v from "./v.png";
+import y from "./y.png";
+import z from "./z.png";
+
+export {
+  aa,
+  ab,
+  e,
+  f,
+  ga,
+  gb,
+  gc,
+  gd,
+  h,
+  ja,
+  jb,
+  na,
+  nb,
+  ra,
+  rb,
+  t,
+  ua,
+  ub,
+  v,
+  y,
+  z,
+};
