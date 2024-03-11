07ed7dbba5dd4fdc1ab5c31fb40e94f2e1e22a86
rakibul-wdp <rakibul.wdp@gmail.com>
2024-03-12 01:39:00 +0600

    mobile component

---
+++ client/src/components/home/Home.tsx
@@ -0,0 +1,13 @@
+import AllMobiles from "./AllMobiles";
+import Banner from "./Banner";
+
+const Home = () => {
+  return (
+    <div>
+      <Banner />
+      <AllMobiles />
+    </div>
+  );
+};
+
+export default Home;
