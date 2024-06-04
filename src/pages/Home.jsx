eee468fb3e928b09a322d6aaed4d6df6b868261b
rakibul-wdp <rakibul.wdp@gmail.com>
2024-06-04 16:54:05 +0600

    create get puzzle section

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,6 +3,7 @@ import Accordian from "../components/home/Accordian";
 import Banner from "../components/home/Banner";
 import Products from "../components/home/Products";
 import OrderNow from "../components/home/OrderNow";
+import GetPuzzle from "../components/home/GetPuzzle";
 
 const Home = () => {
   const data = useLoaderData();
@@ -12,6 +13,7 @@ const Home = () => {
       <Products data={data} />
       <OrderNow />
       <Accordian />
+      <GetPuzzle />
     </div>
   );
 };
